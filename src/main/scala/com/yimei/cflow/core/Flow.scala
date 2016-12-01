488186cf475902ee00048f65a185ab126b39104c
hary <94093146@qq.com>
2016-12-01 16:35:56 +0800

    core refactored again

--- src/main/scala/com/yimei/cflow/core/Flow.scala
+++ src/main/scala/com/yimei/cflow/core/Flow.scala
@@ -7,36 +7,7 @@ import akka.actor.{Actor, ActorLogging, ActorRef}
 import com.yimei.cflow.core.Flow.{DataPoint, Decision, Edge, State}
 import spray.json.{DefaultJsonProtocol, DeserializationException, JsString, JsValue, RootJsonFormat}
 
-object FlowProtocol extends DefaultJsonProtocol {
 
-  // 日期
-  implicit object DateJsonFormat extends RootJsonFormat[Date] {
-    val formatter = new SimpleDateFormat("yyyyMMdd")
-    override def write(obj: Date) = JsString(formatter.format(obj))
-    override def read(json: JsValue) : Date = json match {
-      case JsString(s) => formatter.parse(s)
-      case _ => throw new DeserializationException("Error info you want here ...")
-    }
-  }
-
-  // 序列化Decision
-  implicit  object DecisionFormat extends RootJsonFormat[Decision] {
-    def write(c: Decision) = JsString(c.toString)
-    def read(value: JsValue) = null
-  }
-
-  // 序列Edge
-  implicit object EdgeFormat extends RootJsonFormat[Edge] {
-    def write(c: Edge) = JsString(c.toString)
-    def read(value: JsValue) = null
-  }
-
-  // 数据点
-  implicit val dataPointFormat = jsonFormat4(DataPoint)
-
-  // 状态
-  implicit val stateFormat = jsonFormat5(State)
-}
 
 
 object Flow {
