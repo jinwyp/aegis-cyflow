76a5c65d38c79436dcd763c155cb1d801ce06283
hary <94093146@qq.com>
2016-12-13 09:36:30 +0800

    updated

---
+++ src/main/scala/com/yimei/cflow/http/GroupRoute.scala
@@ -0,0 +1,130 @@
+package com.yimei.cflow.http
+
+import javax.ws.rs.Path
+
+import akka.actor.ActorRef
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import akka.util.Timeout
+import com.yimei.cflow.integration.ServiceProxy
+import com.yimei.cflow.user.{User, UserProtocol}
+import io.swagger.annotations.{ApiImplicitParams, ApiOperation, ApiResponses, _}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+@Path("/user/:userId")
+class GroupRoute(proxy: ActorRef) extends UserProtocol with SprayJsonSupport {
+
+  implicit val timeout = GroupRoute.userServiceTimeout // todo  why import User.userServiceTimeout does not work
+
+  /**
+    * 创建用户
+    */
+  @ApiOperation(value = "userState", notes = "", nickname = "创建用户", httpMethod = "POST")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(
+      name = "body",
+      value = "创建用户",
+      required = true,
+      dataType = "com.yimei.cflow.user.User.State",
+      paramType = "body"
+    )
+    // new ApiImplicitParam(name = "orgId",     value = "组织Id", required = false, dataType = "string", paramType = "path"),
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "服务器应答", response = classOf[User.State]),
+    new ApiResponse(code = 500, message = "Internal server error")
+  ))
+  def postUser: Route = post {
+    pathPrefix("user" / Segment) { userId =>
+      pathEnd {
+        parameter('userType) { userType =>
+          complete(ServiceProxy.userCreate(proxy, userType, userId))
+        }
+      }
+    }
+  }
+
+  // todo 1: add hierachy info support
+  // todo 2: idempotent processing in backend
+
+  /**
+    * 查询用户
+    *
+    * @return
+    */
+  @ApiOperation(value = "userState", notes = "", nickname = "查询用户状态", httpMethod = "GET")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(
+      name = "body",
+      value = "查询用户状态",
+      required = true,
+      dataType = "com.yimei.cflow.user.User.State",
+      paramType = "body"
+    )
+    // new ApiImplicitParam(name = "orgId",     value = "组织Id", required = false, dataType = "string", paramType = "path"),
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "服务器应答", response = classOf[User.State]),
+    new ApiResponse(code = 500, message = "Internal server error")
+  ))
+  def getUser: Route = get {
+    pathPrefix("user" / Segment) { userId =>
+      pathEnd {
+        parameter("userType") { userType =>
+          complete(ServiceProxy.userQuery(proxy, userType, userId))
+        }
+      }
+    }
+  }
+
+  /**
+    * 查询用户
+    *
+    * @return
+    */
+  @ApiOperation(value = "userState", notes = "", nickname = "查询用户状态", httpMethod = "PUT")
+  @ApiImplicitParams(Array(
+    new ApiImplicitParam(
+      name = "body",
+      value = "查询用户状态",
+      required = true,
+      dataType = "com.yimei.cflow.user.User.State",
+      paramType = "body"
+    )
+    // new ApiImplicitParam(name = "orgId",     value = "组织Id", required = false, dataType = "string", paramType = "path"),
+  ))
+  @ApiResponses(Array(
+    new ApiResponse(code = 200, message = "服务器应答", response = classOf[User.State]),
+    new ApiResponse(code = 500, message = "Internal server error")
+  ))
+  def putUser: Route = put {
+    pathPrefix("user" / Segment) { userId =>
+      pathEnd {
+        parameter("userType") { userType =>
+          val k: Future[User.State] = ServiceProxy.userCreate(proxy, userType, userId)
+          complete("put success")
+        }
+      }
+    }
+  }
+
+  def route: Route = postUser ~ getUser ~ putUser
+}
+
+
+/**
+  * Created by hary on 16/12/2.
+  */
+object GroupRoute {
+
+  implicit val userServiceTimeout = Timeout(2 seconds)
+
+
+  def apply(proxy: ActorRef) = new GroupRoute(proxy)
+
+  def route(proxy: ActorRef): Route = GroupRoute(proxy).route
+}
+
