53066ee5e26b9ff0ab31ac2b5529150c7812e648
hary <94093146@qq.com>
2016-12-15 14:38:24 +0800

    updated

--- src/main/scala/com/yimei/cflow/graph/cang/CangConfig.scala
+++ src/main/scala/com/yimei/cflow/graph/cang/CangConfig.scala
@@ -10,27 +10,33 @@ object CangConfig {
     */
   val point_A = "pa"
   val point_B = "pb"
+  val point_X = "px"
+  val point_Y = "py"
 
 
   /**
-    *  所有数据点的描述
+    * 所有数据点的描述
     */
-  val pointDescription = Map[String, String]()
+  val pointDescription = Map[String, String](
+    point_A -> "xxxxx",
+    point_B -> "xxxxx"
+  )
 
   /**
     * 数据采集点Actor名称
     */
-  val data_X = "X"
-  val data_Y = "Y"
+  val data_AB = "AB"
 
   /**
-    *  数据采集点与数据点对应关系
+    * 数据采集点与数据点对应关系
     */
-  val dataPointMap: Map[String, Array[String]] = _
+  val dataPointMap: Map[String, Array[String]] = Map(
+    data_AB -> Array(point_A, point_B)
+  )
 
 
   /**
-    *  所有用户任务
+    * 所有用户任务
     */
   val task_A = "A"
 
@@ -38,6 +44,8 @@ object CangConfig {
   /**
     * 所有用户任务与数据点的对应关系
     */
+  val taskPointMap: Map[String, Array[String]] = Map(
+    task_A -> Array(point_X,point_Y)
+  )
 
-  val taskPointMap: Map[String, Array[String]]  = _
 }
