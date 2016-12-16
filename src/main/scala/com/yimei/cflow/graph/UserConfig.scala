53637819f48d3d5fd463cabc57222dda327a3672
liuxinjie@123 <liuxinjie@yimei180.com>
2016-12-16 14:24:08 +0800

    flow step

--- src/main/scala/com/yimei/cflow/graph/UserConfig.scala
+++ src/main/scala/com/yimei/cflow/graph/UserConfig.scala
@@ -1,5 +1,7 @@
 package com.yimei.cflow.graph
 
+import scala.collection.immutable.HashMap.HashMap1
+
 /**
   * Created by hary on 16/12/15.
   */
@@ -13,12 +15,30 @@ class UserConfig {
   val jgf_class = "jg" // 监管方
 
 
+  // 融资方运营组
+  val rzf_yewu = "yw"  //融资方业务
+
+  // 监管费运营组
+  val jgf_yewu = "yw"  //监管方业务
+
+  // 港口运营组
+  val gkf_yewu = "yw"  //港口方业务
   // 贸易方运营组
+
   val myf_caiwu = "cw" // 贸易方财务
-  val myf_yewu  = "yw"  // 贸易方业务
+  val myf_yewu  = "yw" // 贸易方业务
 
   // 资金方运营组
   val zjf_caiwu = "cw" // 资金方财务
-  val zjf_yewu  = "yw"  // 资金方业务
+  val zjf_yewu  = "yw" // 资金方业务
+
+
+
+  val roleList  = Map[String, String](
+    jgf_yewu -> "监管业务员员",
+    gkf_yewu -> "港口业务员员",
+    zjf_yewu -> "资金方业务员",
+    zjf_caiwu -> "资金方财务"
+  );
 
 }
--- src/main/scala/com/yimei/cflow/graph/cang/RestJsonProtocol.scala
+++ src/main/scala/com/yimei/cflow/graph/cang/RestJsonProtocol.scala
@@ -1,10 +1,11 @@
 package com.yimei.cflow.graph.cang
 
+import spray.json.DefaultJsonProtocol
+
 /**
   * Created by hary on 16/12/15.
   */
 trait RestJsonProtocol extends DefaultJsonProtocol {
-
 
 
 }
