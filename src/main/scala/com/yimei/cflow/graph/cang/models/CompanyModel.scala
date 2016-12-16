1d0b3322e47ac38d02628675a9602073ec9d31bc
liuxinjie@123 <liuxinjie@yimei180.com>
2016-12-16 16:34:30 +0800

    flow step

--- src/main/scala/com/yimei/cflow/graph/cang/models/CompanyModel.scala
+++
@@ -1,8 +0,0 @@
-package com.yimei.cflow.graph.cang.models
-
-import java.time.LocalDateTime
-import BaseFormatter._
-
-case class AddCompany(name: String, userType: String, createTime: LocalDateTime, createManId: BigInt, lastUpdateTime: LocalDateTime, lastUpdateManId: BigInt)
-
-case class UpdateCompany(id: BigInt, name: String, userType: String, lastUpdateTime: LocalDateTime, lastUpdateManId: BigInt)
