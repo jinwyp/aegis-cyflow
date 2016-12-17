b1737ff3566f11a4efa7e7073c09fd7dcc6389e2
hary <94093146@qq.com>
2016-12-17 20:32:25 +0800

    updated

--- src/main/scala/com/yimei/cflow/core/GraphLoader.scala
+++ src/main/scala/com/yimei/cflow/core/GraphLoader.scala
@@ -1,10 +1,12 @@
 package com.yimei.cflow.core
 
 import java.io.File
+import java.net.URLClassLoader
 import java.util
 import java.util.Map.Entry
 import java.util.function.Consumer
 
+import akka.actor.{ActorRef, Props}
 import com.typesafe.config.{Config, ConfigValue}
 import com.yimei.cflow.config.CoreConfig
 import com.yimei.cflow.core.Flow.{Graph, State}
@@ -20,29 +22,41 @@ object GraphLoader extends CoreConfig {
     val flows = coreConfig.getConfig("flow");
     flows.entrySet().forEach(new Consumer[Entry[String, ConfigValue]] {
       override def accept(t: Entry[String, ConfigValue]): Unit = {
-        val graph = loadGraph(t.getKey)
+
+        val jarFile: String = null // todo:  get from config
+        val graph = loadGraph(t.getKey, jarFile)  // 加载
+
         FlowRegistry.register(t.getKey, graph)  // 注册graph
       }
     })
   }
 
   // 加载deciders
-  def loadDeciders() = {
-
+  def loadDeciders(classLoader: ClassLoader) = {
   }
 
   // 加载actors
-  def loadActors() = {
+  def loadActors(classLoader: ClassLoader) = {
 
   }
 
-  def loadGraph(flowType: String): FlowGraph = {
+  def loadGraph(flowType: String, jarFile: String): FlowGraph = {
 
-    coreConfig.getConfig(s"flow.${flowType}")
+    // 加载器
+    val classLoader: URLClassLoader = new java.net.URLClassLoader(
+      Array(new File(jarFile).toURI.toURL),
+      this.getClass.getClassLoader)
+
+    // 加载决策器
+    loadDeciders(classLoader)
+
+    // 加载自动任务Actor
+    loadActors(classLoader)
 
     // 从配置文件中读取各项配置
     val userTasks: Map[String, Array[String]] = null;
 
+    // 返回流程
     new FlowGraph {
       override def getFlowGraph(state: State): Graph = ???
       override def getAutoTask: Map[String, AutoProperty] = ???
@@ -52,10 +66,17 @@ object GraphLoader extends CoreConfig {
     }
   }
 
-
+  // 测试, 先看能否动态加载
   def kload: FlowGraph = {
-    var module = this.getClass.getClassLoader.loadClass("com.yimei.cflow.graph.ying.YingGraph$")
-    module.getField("MODULE$").get(null).asInstanceOf[FlowGraph]
+    val name = "com.yimei.cflow.graph.ying.YingGraph$"
+    val module = this.getClass.getClassLoader.loadClass(name)
+    try {
+      module.getField("MODULE$").get(null).asInstanceOf[FlowGraph]
+    } catch {
+      case e: java.lang.ClassCastException =>
+        printf(" - %s is not Module\n", module)
+        throw e
+    }
   }
 
 
