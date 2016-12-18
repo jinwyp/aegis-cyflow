1e0f8b13eee9c4c9424fd8b91022b767d0032393
hary <94093146@qq.com>
2016-12-18 10:17:47 +0800

    update GraphLoader

--- src/main/scala/com/yimei/cflow/core/GraphLoader.scala
+++ src/main/scala/com/yimei/cflow/core/GraphLoader.scala
@@ -56,8 +56,7 @@ object GraphLoader extends CoreConfig {
   def loadGraph(flowType: String, jarFile: String): FlowGraph = {
 
     // 加载器
-    val classLoader: URLClassLoader = new java.net.URLClassLoader(
-      Array(new File(jarFile).toURI.toURL),
+    val classLoader: URLClassLoader = new java.net.URLClassLoader(Array(new File(jarFile).toURI.toURL),
       this.getClass.getClassLoader)
 
     // 加载决策器
@@ -108,33 +107,8 @@ object GraphLoader extends CoreConfig {
   }
 
 
-  def load() = {
-
-    /*
-   * need to specify parent, so we have all class instances
-   * in current context
-   */
+  def getClassLoader() = {
     var classLoader = new java.net.URLClassLoader(Array(new File("module.jar").toURI.toURL),
       this.getClass.getClassLoader)
-
-    /*
-   * please note that the suffix "$" is for Scala "object",
-   * it's a trick
-   */
-    var clazzExModule = classLoader.loadClass("com.yimei.graph.ying.Ying" + "$")
-
-    /*
-   * currently, I don't know how to check if clazzExModule is instance of
-   * Class[Module], because clazzExModule.isInstanceOf[Class[_]] always
-   * returns true,
-   * so I use try/catch
-   */
-    try {
-      //"MODULE$" is a trick, and I'm not sure about "get(null)"
-      var module = clazzExModule.getField("MODULE$").get(null).asInstanceOf[FlowGraph]
-    } catch {
-      case e: java.lang.ClassCastException =>
-        printf(" - %s is not Module\n", clazzExModule)
-    }
   }
 }
