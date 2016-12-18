b7ea774c8b33de207bfe74da1ae8dfdb6296bc1d
liuxinjie@123 <liuxinjie@yimei180.com>
2016-12-18 20:51:26 +0800

    flow step

--- src/main/scala/com/yimei/cflow/graph/cang/views/UserView.scala
+++ src/main/scala/com/yimei/cflow/graph/cang/views/UserView.scala
@@ -7,13 +7,12 @@ import com.yimei.cflow.graph.cang.models.BaseFormatter._
 
 object UserView extends DefaultJsonProtocol {
 
-  implicit val userListFormat = jsonFormat6(UserList)
-  implicit val userDetailFormat = jsonFormat8(UserDetail)
-
   /** 用户管理,列表字段 **/
   case class UserList(id: Int, username: String, name: String, companyName: String, roleName: String, createTim: Timestamp)
+  implicit val userListFormat = jsonFormat6(UserList)
 
   /** 用户详细信息 **/
   case class UserDetail(id: BigInt, username: String, name: String, email: String, phone: String, companyName: String, roleName: String, createTime: Timestamp)
+  implicit val userDetailFormat = jsonFormat8(UserDetail)
 
 }
