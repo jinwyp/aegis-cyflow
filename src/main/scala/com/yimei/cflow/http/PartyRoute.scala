2b669da60b3fa104c68d4c987b964deaf0124824
qwang1990 <526186564@qq.com>
2016-12-19 15:10:32 +0800

    fix

--- src/main/scala/com/yimei/cflow/http/PartyRoute.scala
+++ src/main/scala/com/yimei/cflow/http/PartyRoute.scala
@@ -1,8 +1,31 @@
 package com.yimei.cflow.http
 
+import akka.actor.ActorRef
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+import com.yimei.cflow.util.DBUtils._
 /**
   * Created by hary on 16/12/19.
   */
-class PartyRoute {
+class PartyRoute(proxy: ActorRef) {
 
+  def getParty:Route  = get {
+    (pathPrefix("party")&parameter("limit") & parameter("offset")) { (limit,offset) =>
+        //dbrun()
+
+        complete("success")
+      }
+    }
+
+
+}
+
+object PartyRoute {
+
+  //implicit val userServiceTimeout = Timeout(2 seconds)
+
+
+  def apply(proxy: ActorRef) = new UserRoute(proxy)
+
+  def route(proxy: ActorRef): Route = UserRoute(proxy).route
 }
