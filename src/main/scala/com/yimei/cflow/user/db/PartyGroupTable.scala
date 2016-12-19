b37bd63e2795bf143c6945f7b82d080ece628c2d
qwang1990 <526186564@qq.com>
2016-12-19 14:56:24 +0800

    fix

--- src/main/scala/com/yimei/cflow/user/db/PartyGroupTable.scala
+++ src/main/scala/com/yimei/cflow/user/db/PartyGroupTable.scala
@@ -7,20 +7,20 @@ import com.yimei.cflow.config.DatabaseConfig.driver
 /**
   * Created by hary on 16/12/16.
   */
-case class UserGroupEntity(id:Option[Long],party_class:String,gid:String,description:String,ts_c:Timestamp)
-trait UserGroupTable {
+case class PartyGroupEntity(id:Option[Long],party_class:String,gid:String,description:String,ts_c:Timestamp)
+trait PartyGroupTable {
   import driver.api._
 
-  class UserGroup(tag:Tag) extends Table[UserGroupEntity](tag,"user_group"){
+  class PartyGroup(tag:Tag) extends Table[PartyGroupEntity](tag,"party_group"){
     def id = column[Option[Long]]("id", O.PrimaryKey, O.AutoInc)
     def party_class = column[String]("party_class")
     def gid = column[String]("gid")
     def description = column[String]("description")
     def ts_c = column[Timestamp]("ts_c")
 
-    def * = (id,party_class,gid,description,ts_c) <> (UserGroupEntity.tupled,UserGroupEntity.unapply)
+    def * = (id,party_class,gid,description,ts_c) <> (PartyGroupEntity.tupled,PartyGroupEntity.unapply)
   }
 
-  protected val groupGroup = TableQuery[UserGroup]
+  protected val groupGroup = TableQuery[PartyGroup]
 
 }
