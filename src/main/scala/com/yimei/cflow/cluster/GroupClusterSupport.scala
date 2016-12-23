1367a21312f4989e7af757049fdac1475f067145
hary <94093146@qq.com>
2016-12-23 15:55:39 +0800

    updated

--- src/main/scala/com/yimei/cflow/cluster/GroupClusterSupport.scala
+++ src/main/scala/com/yimei/cflow/cluster/GroupClusterSupport.scala
@@ -1,22 +1,22 @@
-package com.yimei.cflow.core
+package com.yimei.cflow.cluster
 
 import akka.cluster.sharding.ShardRegion
-import com.yimei.cflow.api.models.flow.Command
+import com.yimei.cflow.api.models.group.Command
 
 /**
   * Created by hary on 16/12/16.
   */
-object FlowClusterSupport {
+object GroupClusterSupport {
   // for cluster
   val extractEntityId: ShardRegion.ExtractEntityId = {
-    case cmd: Command => (cmd.flowId, cmd)
+    case cmd: Command => (cmd.ggid, cmd)
   }
 
   val numberOfShards = 100
 
   val extractShardId: ShardRegion.ExtractShardId = {
-    case cmd: Command => (cmd.flowId.hashCode % numberOfShards).toString
+    case cmd: Command => (cmd.ggid.hashCode % numberOfShards).toString
   }
 
-  val shardName = "flow"
+  val shardName = "group"
 }
