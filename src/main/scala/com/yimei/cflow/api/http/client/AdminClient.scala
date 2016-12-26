20dde187a8330aa7383d021db330bfa93edbe065
qwang1990 <526186564@qq.com>
2016-12-26 16:32:14 +0800

    fix

--- src/main/scala/com/yimei/cflow/api/http/client/AdminClient.scala
+++ src/main/scala/com/yimei/cflow/api/http/client/AdminClient.scala
@@ -1,8 +1,16 @@
 package com.yimei.cflow.api.http.client
+import com.yimei.cflow.api.util.HttpUtil._
+
+import scala.concurrent.Future
+
 
 /**
   * Created by hary on 16/12/23.
   */
 trait AdminClient {
+
+  def createFlow(party:String,instance_id:String,user_id:String,flowType:String,initdata:Map[String,String]): Future[String] = {
+    sendRequest(path = "api/flow",paramters = Map("flowType"->flowType),pathVariables = Array(party,instance_id,user_id),method = "post")
+  }
 
 }
