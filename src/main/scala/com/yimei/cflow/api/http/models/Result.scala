cfdbce9b3acc6da79325f832d6a620c3b5cb02d1
qwang1990 <526186564@qq.com>
2016-12-27 11:33:52 +0800

    temp

--- src/main/scala/com/yimei/cflow/api/http/models/Result.scala
+++ src/main/scala/com/yimei/cflow/api/http/models/Result.scala
@@ -1,6 +1,8 @@
 package com.yimei.cflow.api.http.models
 
-import spray.json.{DefaultJsonProtocol, JsonFormat}
+import com.yimei.cflow.graph.cang.exception.BusinessException
+import spray.json
+import spray.json.{DefaultJsonProtocol, JsArray, JsNumber, JsObject, JsString, JsValue, JsonFormat, RootJsonFormat}
 
 /**
   * Created by wangqi on 16/12/27.
@@ -12,8 +14,41 @@ object ResultModel {
   case class Error(code:Int,message:String,field:String)
 
   trait ResultProtocol extends DefaultJsonProtocol {
-    implicit def metaFormat = jsonFormat4(Meta)
-    implicit def errorFormat = jsonFormat3(Error)
+
+    implicit object MetaFormat extends RootJsonFormat[Meta] {
+      override def write(obj: Meta): JsValue = {
+        if (obj==null){
+          JsString("null")
+        } else {
+          JsArray(JsNumber(obj.total),JsNumber(obj.count),JsNumber(obj.offset),JsNumber(obj.page))
+        }
+      }
+
+      override def read(json: JsValue): Meta = json match {
+        case JsArray(Vector(JsNumber(total),JsNumber(count),JsNumber(offset),JsNumber(page))) =>
+          Meta(total.toInt,count.toInt,offset.toInt,page.toInt)
+        case _ =>throw new BusinessException("Meta 序列化错误")
+      }
+    }
+
+    implicit object errFormat extends RootJsonFormat[Error] {
+      override def write(obj: Error): JsValue = {
+        if(obj==null){
+          JsString("null")
+        } else {
+          JsArray(JsNumber(obj.code),JsString(obj.message),JsString(obj.field))
+        }
+      }
+
+      override def read(json: JsValue): Error =json match {
+        case JsArray(Vector(JsNumber(code),JsString(message),JsString(field))) =>
+          Error(code.toInt,message,field)
+        case _ =>throw new BusinessException("error 序列化错误")
+      }
+    }
+
+    //implicit def metaFormat = jsonFormat4(Meta)
+    //implicit def errorFormat = jsonFormat3(Error)
     implicit def resultEntityFormat[A :JsonFormat] = jsonFormat4(Result.apply[A])
   }
 
--- src/main/scala/com/yimei/cflow/graph/cang/CangGraphJar.scala
+++ src/main/scala/com/yimei/cflow/graph/cang/CangGraphJar.scala
@@ -1,8 +1,17 @@
 package com.yimei.cflow.graph.cang
 
+import com.yimei.cflow.api.models.flow.{Arrow, State}
+
 /**
   * Created by wangqi on 16/12/26.
   */
 object CangGraphJar {
+
+  val ArrowFail = Arrow("fail", None)
+  val ArrowSuccess = Arrow("success", None)
+
+  def V1(state: State) = {
+      Seq(ArrowFail)
+  }
 
 }
