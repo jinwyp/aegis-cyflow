2577a4fad8cfd67dcd0e6ddad998da9c31281d06
hary <94093146@qq.com>
2016-12-29 14:10:51 +0800

    updated

--- src/main/resources/db/migration/V1__Init.sql
+++ src/main/resources/db/migration/V1__Init.sql
@@ -44,6 +44,7 @@ create table deploy(
   id BIGINT not null auto_increment,
   flow_type varchar(64) not null,
   jar blob(104857600) not null,   -- 100M
+  enable bool not null,           -- 激活
   ts_c timestamp default current_timestamp,
   PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -54,7 +55,6 @@ create table party_class (
   id BIGINT not null auto_increment,
   class_name varchar(32),       -- 参与方类别名称     todo: 应该改为三位编码
   description varchar(64) ,    -- 参与方类别描述
-  enable BOOL,                 -- 激活
   PRIMARY KEY (id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 -- class_name 唯一索引
