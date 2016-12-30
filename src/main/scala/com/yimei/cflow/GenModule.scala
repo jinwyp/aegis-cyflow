04c82643de43b52f1672843a7e13db050f1e8ed0
liuxinjie@123 <liuxinjie@yimei180.com>
2016-12-30 11:36:01 +0800

    genmodule

--- src/main/scala/com/yimei/cflow/GenModule.scala
+++ src/main/scala/com/yimei/cflow/GenModule.scala
@@ -25,13 +25,53 @@ object GenModule extends App with GraphConfigProtocol {
     .parseJson
     .convertTo[GraphConfig]
 
-  val rootPath = "./tmp"
-  val file: File = new File(rootPath)
+  val rootDir = "./tmp"
+  val projectDir = "/project"
+  val buildProperties = "/build.properties"
+  val pluginsSbt = "/plugins.sbt"
+  val srcDir = "/src"
+  val mainDir = "/main"
+  val resourceDir = "/resources"
+  val flowJson = "/flow.json"
+  val buildSbt = "/build.sbt"
+  val configScala = "/Config.scala"
+  val templateGraphJarScala = "/TemplateGraphJar.scala"
+  val scalaDir = "/scala"
+  var file: File = new File(rootDir)
   if (!file.exists()) file.mkdir()
 
-
-  graphConfig.graphJar
-  println(graphConfig)
+  println(graphConfig.graphJar)
+  val graphJarStr = graphConfig.graphJar
+  val nameArray: Array[String] = graphJarStr.split('.')
+  var directoryName = "/" + nameArray(nameArray.length - 2)
+  var jarName = nameArray(nameArray.length - 1)
+  var jarDirName = "/" + graphJarStr.substring(0, graphJarStr.length - directoryName.length - jarName.length - 1).replace(".", "-")
+  file = new File(rootDir + directoryName)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + projectDir)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + projectDir + buildProperties)
+  if (!file.exists()) file.createNewFile()
+  file = new File(rootDir + directoryName + projectDir + pluginsSbt)
+  if (!file.exists()) file.createNewFile()
+  file = new File(rootDir + directoryName + srcDir)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + srcDir + mainDir)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + srcDir + mainDir + resourceDir)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + srcDir + mainDir + resourceDir + flowJson)
+  if (!file.exists()) file.createNewFile()
+  file = new File(rootDir + directoryName + srcDir + mainDir + scalaDir)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + srcDir + mainDir + scalaDir + jarDirName)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + srcDir + mainDir + scalaDir + jarDirName + directoryName)
+  if (!file.exists()) file.mkdir()
+  file = new File(rootDir + directoryName + srcDir + mainDir + scalaDir + jarDirName + directoryName + configScala)
+  if (!file.exists()) file.createNewFile()
+  file = new File(rootDir + directoryName + srcDir + mainDir + scalaDir + jarDirName + directoryName + templateGraphJarScala)
+  if (!file.exists()) file.createNewFile()
 
 
 
