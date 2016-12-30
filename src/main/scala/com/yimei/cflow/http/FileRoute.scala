233f5467684e8121e818052e4286c8b20ebe401b
liuxinjie@123 <liuxinjie@yimei180.com>
2016-12-30 10:06:00 +0800

    file

--- src/main/scala/com/yimei/cflow/http/FileRoute.scala
+++ src/main/scala/com/yimei/cflow/http/FileRoute.scala
@@ -1,37 +1,51 @@
 package com.yimei.cflow.http
 
-import java.io.{File, FileOutputStream}
+import java.io.{BufferedOutputStream, FileOutputStream}
+import java.net.{HttpURLConnection, URL}
 import java.util.UUID
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.{HttpResponse, Multipart, StatusCodes}
+import akka.http.scaladsl.model.Multipart
 import akka.http.scaladsl.model.Multipart.FormData.BodyPart
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.stream.scaladsl.FileIO
 import akka.util.ByteString
 import com.yimei.cflow.config.ApplicationConfig
 import com.yimei.cflow.graph.cang.models.CangFlowModel.FileObj
 
-import scala.concurrent.{Future, future}
+import scala.concurrent.Future
 
 class FileRoute extends ApplicationConfig with SprayJsonSupport {
-
-  val localPath = coreConfig.getString("filePath") + "cang/"
+  val rootPath = coreConfig.getString("filePath")
+  val localPath = rootPath + "cang/"
 
   import scala.concurrent.duration._
+
+  def downloadFile: Route = get {
+    pathPrefix("file" / "download") {
+      pathEnd {
+        parameter("url") { url =>
+          if (url == null || !url.startsWith(rootPath)) {
+            complete("error")
+          }
+          complete("ok")
+        }
+      }
+    }
+  }
+
 
   def uploadFile: Route = post {
     pathPrefix("file" / "upload") {
       pathEnd {
         entity(as[Multipart.FormData]) { fileData =>
           val result: Future[Map[String, String]] = fileData.parts.mapAsync[(String, String)](1) {
-            case file :
+            case file:
               BodyPart if file.name == "file" =>
               val fileName = UUID.randomUUID().toString + "-" + file.getFilename().get()
               val filePath = localPath + fileName
               processFile(filePath, fileData)
-              Future ("url" -> filePath)
+              Future("url" -> filePath)
             case data: BodyPart => data.toStrict(2.seconds)
               .map(strict => data.name -> strict.entity.data.utf8String)
           }.runFold(Map.empty[String, String])((map, tuple) => map + tuple)
@@ -40,7 +54,8 @@ class FileRoute extends ApplicationConfig with SprayJsonSupport {
               val url = data.get("url").get
               val originName = data.get("name").get
               FileObj(url.replace(localPath, ""), originName, url)
-            }}
+            }
+            }
           }
         }
       }
@@ -60,11 +75,13 @@ class FileRoute extends ApplicationConfig with SprayJsonSupport {
     }.runFold(0)(_ + _.length)
   }
 
-  def route: Route = uploadFile
+
+  def route: Route = downloadFile ~ uploadFile
 }
 
 object FileRoute {
 
-  def apply(): FileRoute = new FileRoute()
   def route: Route = FileRoute().route
+
+  def apply(): FileRoute = new FileRoute()
 }
