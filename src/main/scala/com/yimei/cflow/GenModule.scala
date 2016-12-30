6a33a37e36ba13cc9b96ef34c006af5c8e4ffebb
liuxinjie@123 <liuxinjie@yimei180.com>
2016-12-30 19:12:00 +0800

    modules

--- src/main/scala/com/yimei/cflow/GenModule.scala
+++ src/main/scala/com/yimei/cflow/GenModule.scala
@@ -12,12 +12,6 @@ import scala.io.Source
 /**
   * Created by hary on 16/12/29.
   */
-
-//
-// todo xj
-// 生成类似于类路径下 template目录下的整个目录
-//
-//
 object GenModule extends App with GraphConfigProtocol {
 
   val classLoader = this.getClass.getClassLoader
