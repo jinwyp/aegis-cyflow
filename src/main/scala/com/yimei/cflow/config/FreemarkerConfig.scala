166f05cdcf5bd538fbc2916a18901ea331b36689
hary <94093146@qq.com>
2017-01-03 16:32:33 +0800

    updated

--- src/main/scala/com/yimei/cflow/config/FreemarkerConfig.scala
+++ src/main/scala/com/yimei/cflow/config/FreemarkerConfig.scala
@@ -9,30 +9,13 @@ import freemarker.template.{Configuration, TemplateExceptionHandler}
   */
 object FreemarkerConfig extends CoreConfig {
 
-  // Create your Configuration instance, and specify if up to what FreeMarker
-  // version (here 2.3.25) do you want to apply the fixes that are not 100%
-  // backward-compatible. See the Configuration JavaDoc for details.
   val ftl = new Configuration(Configuration.VERSION_2_3_23);
-
-  // Specify the source where the template files come from. Here I set a
-  // plain directory for it, but non-file-system sources are possible too:
   ftl.setDirectoryForTemplateLoading(new File("src-cang/ftl"));
-
-  // Set the preferred charset template files are stored in. UTF-8 is
-  // a good choice in most applications:
   ftl.setDefaultEncoding("UTF-8");
-
-  // Sets how errors will appear.
-  // During web page *development* TemplateExceptionHandler.HTML_DEBUG_HANDLER is better.
   ftl.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-
-  // Don't log exceptions inside FreeMarker that it will thrown at you anyway:
   ftl.setLogTemplateExceptions(false);
 
-
-  def apply(template: String) = ftl.getTemplate(template);
-
-  val staticPath = "asdfasdf/asfasdf"
+  val staticPath = coreConfig.getString("cang.ftl")
 
   def render(template: String, data: Option[Map[String, String]] = None ) = {
     val os = new ByteArrayOutputStream()
