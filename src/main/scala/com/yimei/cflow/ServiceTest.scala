48f02dd6aebc347955f42b66006e3691e768b4ba
hary <94093146@qq.com>
2017-01-04 12:04:01 +0800

    updated

--- src/main/scala/com/yimei/cflow/ServiceTest.scala
+++ src/main/scala/com/yimei/cflow/ServiceTest.scala
@@ -10,7 +10,7 @@ import com.yimei.cflow.config.GlobalConfig._
 import com.yimei.cflow.engine.graph.GraphLoader
 import com.yimei.cflow.engine.routes.{AutoRoute, EditorRoute}
 import com.yimei.cflow.engine.{DaemonMaster, FlowRegistry}
-import com.yimei.cflow.asset.AssetRoute$
+import com.yimei.cflow.asset.AssetRoute
 import com.yimei.cflow.graph.cang.routes._
 import com.yimei.cflow.http._
 import com.yimei.cflow.organ.routes._
