6ccb258e0dc7481aac283c31937f9f82c0beb85a
liuxinjie@123 <liuxinjie@yimei180.com>
2017-01-06 17:50:44 +0800

    design

--- src/main/scala/com/yimei/cflow/engine/routes/DeployRoute.scala
+++ src/main/scala/com/yimei/cflow/engine/routes/DeployRoute.scala
@@ -3,13 +3,13 @@ package com.yimei.cflow.engine.routes
 import akka.actor.ActorRef
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server._
+import com.mysql.cj.jdbc.Blob
+import com.yimei.cflow.api.models.database.FlowDBModel.DeployEntity
 import com.yimei.cflow.config.CoreConfig
+import com.yimei.cflow.engine.routes.DeployObject.SaveDeploy
 
 import scala.concurrent.Future
 
-/**
-  * Created by hary on 16/12/28.
-  */
 class DeployRoute (proxy: ActorRef) extends CoreConfig {
 
 
@@ -34,7 +34,19 @@ class DeployRoute (proxy: ActorRef) extends CoreConfig {
     }
   }
 
-  def route: Route = ???
+  def saveDeploy = post {
+    path("deploy") {
+      parameter("id".as[Long].?) { id =>
+        entity(as[SaveDeploy]) { deploy =>
+          //val jar = deploy.jar.getBytes
+          val deployEntity = DeployEntity(id, deploy.flow_type, jar, true, None)
+
+        }
+      }
+    }
+  }
+
+  def route: Route = deploy ~ saveDeploy
 }
 
 
