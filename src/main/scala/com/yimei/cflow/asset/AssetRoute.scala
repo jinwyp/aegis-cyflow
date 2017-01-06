7781aa89944f214b1dc91da7edad5a4ac798d4c4
liuxinjie@123 <liuxinjie@yimei180.com>
2017-01-06 15:53:25 +0800

    file

--- src/main/scala/com/yimei/cflow/asset/AssetRoute.scala
+++ src/main/scala/com/yimei/cflow/asset/AssetRoute.scala
@@ -4,7 +4,7 @@ import java.io.FileOutputStream
 import java.util.UUID
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.Multipart
+import akka.http.scaladsl.model.{Multipart, StatusCodes}
 import akka.http.scaladsl.model.Multipart.FormData.BodyPart
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
@@ -14,6 +14,7 @@ import com.yimei.cflow.api.util.DBUtils.dbrun
 import com.yimei.cflow.asset.db.AssetTable
 import com.yimei.cflow.config.CoreConfig
 import com.yimei.cflow.config.DatabaseConfig._
+import com.yimei.cflow.exception.DatabaseException
 import com.yimei.cflow.graph.cang.exception.BusinessException
 import com.yimei.cflow.graph.cang.models.CangFlowModel.FileObj
 
@@ -34,13 +35,16 @@ class AssetRoute extends CoreConfig with AssetTable with SprayJsonSupport {
     */
   def downloadFile: Route = get {
     path("file" / Segment) { id =>
-      println(" ------ abc -------- " + id)
-      val url = dbrun(assetClass.filter(f => f.asset_id === id).result.head).map(f => f.url)
-      println(" ------ url -------- " + url)
+      val file: Future[AssetEntity] = dbrun(assetClass.filter(f => f.asset_id === id).result.head) recover {
+        case _ => throw new DatabaseException("该文件不存在")
+      }
+      val url = for {
+        f <- file
+      } yield f.url
       getFromFile(new File(fileRootPath + url))
+      complete(StatusCodes.OK)
     }
   }
-
 
   /**
     * POST asset/    -- 上传文件:
---
+++ src/main/scala/com/yimei/cflow/engine/routes/EditorObject.scala
@@ -0,0 +1,17 @@
+package com.yimei.cflow.engine.routes
+
+
+import java.sql.Timestamp
+
+import spray.json.DefaultJsonProtocol
+
+object EditorObject extends DefaultJsonProtocol {
+  import com.yimei.cflow.graph.cang.models.BaseFormatter.TimeStampJsonFormat
+
+  case class AddDesign(id: Option[Long], name: String, json: Option[String], meta: Option[String])
+  implicit val addDesignFormat = jsonFormat4(AddDesign)
+
+  case class DesignDetail(id: Option[Long], name: String, json: Option[String], meta: Option[String], ts_c: Timestamp)
+  implicit val designDetailFormat = jsonFormat5(DesignDetail)
+
+}
