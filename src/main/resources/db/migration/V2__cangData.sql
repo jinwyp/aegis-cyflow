79eca2e7b8b5f8996a5c94b75cc43015bb9527f3
qwang1990 <526186564@qq.com>
2017-01-16 13:02:48 +0800

    fix

--- src/main/resources/db/migration/V2__cangData.sql
+++ src/main/resources/db/migration/V2__cangData.sql
@@ -8,7 +8,7 @@ INSERT INTO `cyflow`.`party_class` (`class_name`, `description`) VALUES ('system
 -- --贸易方公司（现在至于易煤网）
 -- INSERT INTO `cyflow`.`party_instance` (`party_class`, `instance_id`, `party_name`) VALUES ('trader', '88888888', '孟州市至信有限责任公司');
 --
-INSERT INTO `cyflow`.`party_instance` (`party_class`, `instance_id`, `party_name`) VALUES ('systemAdmin', '00000000', '管理员');
+
 --
 --
 --添加组（贸易商业务，贸易商财务，资金方业务，资金方财务）
@@ -35,6 +35,7 @@ INSERT INTO `cyflow`.`party_group` (`party_class`, `gid`, `description`) VALUES 
 INSERT INTO `party_instance` (`id`, `party_class`, `instance_id`, `party_name`, `disable`, `ts_c`)
 VALUES
 	(1, 'trader', '1822', '上海瑞易供应链管理有限公司', 0, '2017-01-15 19:59:11');
+
 
 --贸易商业务和财务
 INSERT INTO `party_user` (`id`, `party_id`, `user_id`, `username`, `password`, `phone`, `email`, `name`, `disable`, `ts_c`)
@@ -67,3 +68,5 @@ VALUES
 	(3, 2, '1', '99999', '2017-01-12 16:26:02'),
 	(4, 2, '2', '10101', '2017-01-12 16:26:02');
 
+INSERT INTO `cyflow`.`party_instance` (`party_class`, `instance_id`, `party_name`) VALUES ('systemAdmin', '00000000', '管理员');
+INSERT INTO `cyflow`.`party_user` (`party_id`,`user_id`, `password`, `phone`, `email`, `name`,`username`) VALUES ('3','00000', '123456', '13800000001', 'systemAdmin@1.com', 'systemAdmin1', 'admin1');
