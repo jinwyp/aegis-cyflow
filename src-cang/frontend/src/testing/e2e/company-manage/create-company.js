9ca475905b870a46ea527de19ad274dfa2140d7d
gonghuihui <gonghuihui@yimei180.com>
2017-01-16 10:16:09 +0800

    修改testService

---
+++ src-cang/frontend/src/testing/e2e/company-manage/create-company.js
@@ -0,0 +1,12 @@
+/**
+ * Created by gonghuihui on 17/1/11.
+ */
+
+describe('仓押首页',function(){
+    var loginUsernameInput=element(by.id('inputUsername'));
+    var loginPasswordInput=element(by.id('inputPassword'));
+    var loginButton=element(by.partialButtonText('登录'));
+    beforeAll(function(){
+        browser.get('');
+    })
+})
