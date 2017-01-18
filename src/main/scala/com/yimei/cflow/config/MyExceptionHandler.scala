db6837482054e337abd8cfa8632073da475390b5
mayflower-xianglei <fighter_xl@sina.com>
2017-01-18 15:22:52 +0800

    fix

--- src/main/scala/com/yimei/cflow/config/MyExceptionHandler.scala
+++ src/main/scala/com/yimei/cflow/config/MyExceptionHandler.scala
@@ -3,14 +3,17 @@ package com.yimei.cflow.config
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.model.{HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.MediaTypes._
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.ExceptionHandler
+import akka.util.ByteString
 import com.yimei.cflow.api.http.models.ResultModel._
 import com.yimei.cflow.exception.DatabaseException
 import com.yimei.cflow.graph.cang.exception.BusinessException
 import spray.json._
 import com.yimei.cflow.config.CoreConfig._
+import akka.http.scaladsl.model._
 
 /**
   * Created by wangqi on 16/12/27.
@@ -26,18 +29,18 @@ trait MyExceptionHandler extends ResultProtocol with SprayJsonSupport {
     case e:BusinessException =>
       extractUri { uri =>
         log.error("Request to {} could not be handled normally!! error:{} ",uri,e)
-        complete(HttpResponse(StatusCodes.BadRequest,entity = Result(data = Some("error"),success = false,error = Some(Error(409,e.message,""))).toJson.toString ))
+        complete(HttpResponse(StatusCodes.BadRequest,entity = HttpEntity(`application/json`, ByteString(Result(data = Some("error"),success = false,error = Some(Error(409,e.message,""))).toJson.toString, "UTF-8")) ))
       }
 
     case e:DatabaseException =>
       extractUri { uri =>
         log.error("Request to {} could not be handled normally!! error:{} ",uri,e)
-        complete(HttpResponse(StatusCodes.BadRequest,entity = Result(data = Some("error"),success = false,error = Some(Error(409,e.message,""))).toJson.toString ))
+        complete(HttpResponse(StatusCodes.BadRequest,entity = HttpEntity(`application/json`, ByteString(Result(data = Some("error"),success = false,error = Some(Error(409,e.message,""))).toJson.toString ))))
       }
     case e =>
       extractUri { uri =>
         log.error("Request to {} could not be handled normally!! error:{} ",uri,e)
-        complete(HttpResponse(StatusCodes.InternalServerError,entity = Result(data = Some("error"),success = false,error = Some(Error(500,"系统错误",""))).toJson.toString ))
+        complete(HttpResponse(StatusCodes.InternalServerError,entity = HttpEntity(`application/json`, ByteString(Result(data = Some("error"),success = false,error = Some(Error(500,"系统错误",""))).toJson.toString ))))
       }
   }
 }
